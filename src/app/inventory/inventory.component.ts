b73a002468ab347b75e228b2e403bb51def63ac6
Adam Ochotnicki <adam.ochotnicki@mail.itsligo.ie>
2019-02-26 01:51:25 +0000

    some progress :)

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,13 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from '../product';
 
 @Component({
   selector: 'app-inventory',
-  templateUrl: './inventory.component.html',
+  template:
+  `
+    <div class="app-inventory">
+      <app-product-list
+        [productList]="products"
+        (onProductsSelected)="prductWasSelected($event)">
+      </app-product-list>
+    </div>
+  `,
   styleUrls: ['./inventory.component.css']
 })
 export class InventoryComponent implements OnInit {
+  products:Product[];
+  constructor() { 
+    this.products = [new Product('MYSHOES',
+                              'Black running shoes',
+                              'assets/images/products/black-shoes.jpg',
+                              ['Men', 'Shoes', 'Running Shoes'],
+                              109.99),
+                    new Product('NEATOJACKET',
+                              'Blue Jacket',
+                              'assets/images/products/blue-jacket.jpg',
+                              ['Women', 'Apparel', 'Jackets and Vests'],
+                              238.99),
+                      new Product('NICEHAT',
+                              'A Nice Black Hat',
+                              'assets/images/products/black-hat.jpg',
+                              ['Men', 'Accessories', 'Hats'],
+                              29.99)];
+  }
 
-  constructor() { }
+  productWasSelected(product:Product): void {
+    console.log('Product clicked: ', product);
+  }
 
   ngOnInit() {
   }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,13 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from '../product';
+import {EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  inputs: ['productList'],
+  outputs: ['onProductSelected'],
+  template:
+  `
+    <div class="ui items">
+    <app-product-row
+      *ngFor="let myProduct of productList"
+      [product]="myProduct"
+      (click)='clicked(myProduct)'
+      [class.selected]="isSelected(myProduct)">
+    </app-product-row>
+    </div>
+  `,
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
+  /**
+   * @input productList - the Product[] passed to this component
+   */
+  productList:Product[]
+
+  /**
+   * @output onProductSelected - outputs the current Product whenever a new product is selected
+   */
+  onProductSelected:EventEmitter<Product>
+
+  private currentProduct:Product;
   constructor() { }
+
+  clicked(product:Product):void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product:Product):boolean {
+    if (!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
+  }
 
   ngOnInit() {
   }
